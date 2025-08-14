987bc02f007778115692ee17980b5f6fbf475422
Nomthandazo Ngcobo <nomthandazongcobo24@icloud.com>
2025-08-14 11:45:17 +0200

    : Modularize conversion functions and separate I/O logic

---
+++ Commit 5.cpp
@@ -0,0 +1,11 @@
+namespace Converter {
+    string decimalToBinary(int decimal) { /* existing code */ }
+    int binaryToDecimal(const string& binary) { /* existing code */ }
+    string decimalToHex(int decimal) { /* existing code */ }
+    int hexToDecimal(const string& hex) { /* existing code */ }
+    bool isValidBinary(const string& binary) { /* as above */ }
+    bool isValidHex(const string& hex) { /* as above */ }
+}
+
+// Usage in main:
+cout << "Binary: " << Converter::decimalToBinary(decimal) << endl;
